513ea0ed0cb6712210c5d2b0f6de28384c977d85
ahmam1618-ai <ahmam1618@gmail.com>
2026-05-16 18:36:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -105,5 +105,4 @@ const server = http.createServer((req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.env.PORT || 3000;
 server.listen(PORT, () => console.log('Mock Server running on port ' + PORT));
